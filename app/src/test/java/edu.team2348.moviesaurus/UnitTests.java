ad2eb8d2506bff432388db9e5abaf9690c13523f
Farhan Tejani <farhantejani@gmail.com>
2016-04-08 11:23:33 -0400

    Added unit tests for Movie.equals

--- app/src/test/java/edu.team2348.moviesaurus/UnitTests.java
+++ app/src/test/java/edu.team2348.moviesaurus/UnitTests.java
@@ -15,6 +15,9 @@ import java.util.Map;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -23,17 +26,11 @@ import static org.junit.Assert.assertEquals;
 public class UnitTests {
 
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
-
-    @Test
     public void mapConversion() throws JSONException, ParseException {
         JsonArray storeRating = new JsonArray();
         Random rand = new Random();
-        ParseObject.registerSubclass(Movie.class);
         Map<String, Double> expected = new HashMap<>();
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 10; i++) {
             double r = rand.nextDouble() * 4 + 1;
             JsonObject o = new JsonObject();
             o.addProperty("user", String.valueOf((char) (i + 65)));
@@ -49,5 +46,18 @@ public class UnitTests {
             actual.put(user, rating);
         }
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void movieEqualsTest() {
+        ParseObject.registerSubclass(Movie.class);
+        Movie divergent = new Movie("Divergent", "The best movie.", "www.google.com");
+        Movie stillDivergent = new Movie("Divergent", "The best movie.", "www.google.com");
+        Movie duplicate = divergent;
+        Movie nullMovie = null;
+        assertFalse(divergent.equals(nullMovie));
+        assertTrue(divergent.equals(duplicate));
+        assertFalse(divergent.equals(new String("hello")));
+        assertTrue(divergent.equals(stillDivergent));
     }
 }
